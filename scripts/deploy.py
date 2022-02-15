fd37e4c6a1cbad1068018fce43aa92399886dadd
garywackett5 <garywackett5@protonmail.com>
2022-02-15 17:20:20 +0000

    Fix: Updated to deploy our strategy

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,58 +1,29 @@
 from pathlib import Path
 
-from brownie import Strategy, accounts, config, network, project, web3
+from brownie import accounts, config, network, project, web3, Contract, Strategy0xDAOStaker
 from eth_utils import is_checksum_address
 import click
 
-API_VERSION = config["dependencies"][0].split("@")[-1]
-Vault = project.load(
-    Path.home() / ".brownie" / "packages" / config["dependencies"][0]
-).Vault
-
-
-def get_address(msg: str, default: str = None) -> str:
-    val = click.prompt(msg, default=default)
-
-    # Keep asking user for click.prompt until it passes
-    while True:
-
-        if is_checksum_address(val):
-            return val
-        elif addr := web3.ens.address(val):
-            click.echo(f"Found ENS '{val}' [{addr}]")
-            return addr
-
-        click.echo(
-            f"I'm sorry, but '{val}' is not a checksummed address or valid ENS record"
-        )
-        # NOTE: Only display default once
-        val = click.prompt(msg)
-
-
 def main():
-    print(f"You are using the '{network.show_active()}' network")
-    dev = accounts.load(click.prompt("Account", type=click.Choice(accounts.load())))
-    print(f"You are using: 'dev' [{dev.address}]")
+    vault = Contract("0x0fBbf9848D969776a5Eb842EdAfAf29ef4467698")
+    pid = 21
+    strategy_name = "StrategyHECStakerBoo"
+    masterchef = Contract("0x2352b745561e7e6FCD03c093cE7220e3e126ace0")
+    emission_token = Contract("0x5C4FDfc5233f935f20D2aDbA572F770c2E377Ab0")
+    swap_first_step = Contract("0x8D11eC38a3EB5E956B052f67Da8Bdc9bef8Abf3E")
+    auto_sell = True
 
-    if input("Is there a Vault for this strategy already? y/[N]: ").lower() == "y":
-        vault = Vault.at(get_address("Deployed Vault: "))
-        assert vault.apiVersion() == API_VERSION
-    else:
-        print("You should deploy one vault using scripts from Vault project")
-        return  # TODO: Deploy one using scripts from Vault project
+    strategist = accounts.load('yearn')
 
-    print(
-        f"""
-    Strategy Parameters
-
-       api: {API_VERSION}
-     token: {vault.token()}
-      name: '{vault.name()}'
-    symbol: '{vault.symbol()}'
-    """
+    strategy = strategist.deploy(
+        Strategy0xDAOStaker,
+        vault,
+        pid,
+        strategy_name,
+        masterchef,
+        emission_token,
+        swap_first_step,
+        auto_sell,
     )
-    publish_source = click.confirm("Verify source on etherscan?")
-    if input("Deploy Strategy? y/[N]: ").lower() != "y":
-        return
 
-    strategy = Strategy.deploy(vault, {"from": dev}, publish_source=publish_source)
+    print(strategy)
